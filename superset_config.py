e947d1c8d2c5bcf2281aec31c9e610f9bf1a0d9b
Denis Golovanov <dgolovanov@MacBook-Pro-Denis.local>
2025-11-13 13:17:13 +0300

    Fix guest

--- superset_config.py
+++ superset_config.py
@@ -1,3 +1,4 @@
+
 SECRET_KEY = "Z4xY7tQ9vR1sU3pK8mN5wB2eH6cJ0aF3"
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
@@ -6,3 +7,21 @@ DATA_DIR = "/app/superset_home"
 UPLOAD_FOLDER = f"{DATA_DIR}/uploads"
 MAX_CONTENT_LENGTH = 100 * 1024 * 1024
 DEBUG = True
+
+
+from flask_appbuilder.security.manager import AUTH_DB
+
+AUTH_TYPE = AUTH_DB                # используем внутреннюю БД пользователей
+AUTH_ROLE_PUBLIC = 'Gamma'        # роль "только просмотр"
+AUTH_USER_REGISTRATION = True     # разрешаем регистрацию пользователей
+AUTH_USER_REGISTRATION_ROLE = AUTH_ROLE_PUBLIC
+
+
+INITIAL_ADMIN_USER = {
+    "username": "guest",
+    "password": "guest",
+    "first_name": "Guest",
+    "last_name": "User",
+    "email": "guest@example.com",
+    "roles": ["Gamma"]
+}
