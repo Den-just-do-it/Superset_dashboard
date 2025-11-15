9515d60fb19525b155d8914462a08d97ee9e0489
Denis Golovanov <dgolovanov@MacBook-Pro-Denis.local>
2025-11-15 15:34:04 +0300

    Fix

--- superset_config.py
+++ superset_config.py
@@ -1,24 +1,16 @@
-
-
-SECRET_KEY = "jA4qkvIMzFBtT8uaFASnrS3nftBqn315VHl8FOpIKaY7EiRJmIe"
+SECRET_KEY = "supersecretkey"
 
 SQLALCHEMY_DATABASE_URI = "sqlite:////app/superset_home/superset.db"
 
 SUPERSET_HOME = "/app/superset_home"
-
 DATA_DIR = SUPERSET_HOME
 UPLOAD_FOLDER = f"{DATA_DIR}/uploads"
 MAX_CONTENT_LENGTH = 100 * 1024 * 1024
 
-DEBUG = True
-
-
-
 from flask_appbuilder.security.manager import AUTH_DB
-
 AUTH_TYPE = AUTH_DB
-AUTH_ROLE_PUBLIC = "Gamma"
 AUTH_USER_REGISTRATION = True
+AUTH_ROLE_PUBLIC = "Public"
 AUTH_USER_REGISTRATION_ROLE = AUTH_ROLE_PUBLIC
 
 INITIAL_ADMIN_USER = {
@@ -26,18 +18,5 @@ INITIAL_ADMIN_USER = {
     "password": "guest",
     "first_name": "Guest",
     "last_name": "User",
-    "email": "guest@example.com",
-    "roles": ["Gamma"],
+    "email": "guest@example.com"
 }
-
-
-
-DATABASES = {
-    "clickhouse_retail": {
-        # если ClickHouse у тебя локально или в Render — адрес менять здесь
-        "SQLALCHEMY_URI": "clickhouse+http://default:@localhost:8123/retail",
-        "EXTRA": {},
-    }
-}
-
-
